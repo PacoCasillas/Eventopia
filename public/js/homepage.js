b247537af02f811c26fb907eb62ec9041d5699aa
Joao Marchete <elisamarchete@gmail.com>
2023-06-16 14:25:48 -0400

    created fetch for attendees

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,22 +1,36 @@
-// const btnattend = document.querySelectorAll("#btn-attend");
+const btnattend = document.querySelectorAll("#btn-attend");
 const btnFavorite = document.querySelectorAll("#btn-favorite");
 
-// ADD EVENT AS ATTENDEE TO FAVORITE PAGE
-// btnattend.forEach((btn) => {
-//   btn.addEventListener("click", (e) => {
-//     const id = e.target.dataset.id;
-//     const url = `/api/attend/${id}`;
-//     const method = "POST";
-//     fetch(url, { method })
-//       .then((res) => res.json())
-//       .then((data) => {
-//         if (data.success) {
-//           btn.classList.add("btn-success");
-//           btn.innerHTML = "Attending";
-//         }
-//       });
-//   });
-// });
+// ADD TO ATTENDEES
+btnattend.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+    // console.log("id", id);
+
+    fetch("/api/attendees", {
+      method: "POST",
+      body: JSON.stringify({
+        event_id: parseInt(id),
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not OK");
+        }
+        return response.json(); // Parse the response body as JSON
+      })
+      // Here, 'data' contains the parsed response data
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
+});
 
 // ADD TO FAVORITES
 btnFavorite.forEach((btn) => {
