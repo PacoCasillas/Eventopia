9edf06fcdb558625b90e1103db74ca4502b81cee
Joao Marchete <elisamarchete@gmail.com>
2023-06-15 20:08:24 -0400

    created delete route for event -> Create, update and delete event beckend running

--- controllers/api/event-routes.js
+++ controllers/api/event-routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { Event, User } = require("../../models");
 
-// CREATE A NEW EVENT -> http://localhost:3001/api/events
+// CREATE EVENT -> http://localhost:3001/api/events
 router.post("/", async (req, res) => {
   try {
     const eventData = await Event.create({
@@ -22,7 +22,7 @@ router.post("/", async (req, res) => {
   }
 });
 
-// UPDATE AN EVENT -> http://localhost:3001/api/events/:id
+// UPDATE EVENT -> http://localhost:3001/api/events/
 router.put("/", async (req, res) => {
   try {
     const eventData = await Event.update(
@@ -36,13 +36,28 @@ router.put("/", async (req, res) => {
         endDate: req.body.endDate,
         startTime: req.body.startTime,
         endTime: req.body.endTime,
+        created_by: req.session.user_id,
       },
       {
         where: {
-          id: req.params.id,
+          id: req.body.id,
         },
       }
     );
+    res.status(200).json(eventData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// DELETE AN EVENT -> http://localhost:3001/api/events/
+router.delete("/", async (req, res) => {
+  try {
+    const eventData = await Event.destroy({
+      where: {
+        id: req.body.id,
+      },
+    });
     res.status(200).json(eventData);
   } catch (err) {
     res.status(400).json(err);
