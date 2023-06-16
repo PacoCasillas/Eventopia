05760a112725ebaff6dceeccbc7b3b82aa63143a
Joao Marchete <elisamarchete@gmail.com>
2023-06-16 14:18:46 -0400

    created fetch for favorites

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,10 @@ const router = require("express").Router();
 
 const userRoutes = require("./userRoutes");
 const eventRoutes = require("./event-routes");
+const favoriteRoutes = require("./favorite-routes");
 
 router.use("/", userRoutes);
-
 router.use("/events", eventRoutes);
+router.use("/favorites", favoriteRoutes);
 
 module.exports = router;
--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,36 +1,50 @@
-const btnattend = document.querySelectorAll("#btn-attend");
-const btnfavorite = document.querySelectorAll("#btn-favorite");
+// const btnattend = document.querySelectorAll("#btn-attend");
+const btnFavorite = document.querySelectorAll("#btn-favorite");
 
 // ADD EVENT AS ATTENDEE TO FAVORITE PAGE
-btnattend.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const id = e.target.dataset.id;
-    const url = `/api/events/${id}/attend`;
-    const method = "POST";
-    fetch(url, { method })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.success) {
-          btn.classList.add("btn-success");
-          btn.innerHTML = "Attending";
-        }
-      });
-  });
-});
+// btnattend.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     const id = e.target.dataset.id;
+//     const url = `/api/attend/${id}`;
+//     const method = "POST";
+//     fetch(url, { method })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         if (data.success) {
+//           btn.classList.add("btn-success");
+//           btn.innerHTML = "Attending";
+//         }
+//       });
+//   });
+// });
 
-// ADD EVENT AS FAVORITE TO FAVORITE PAGE
-btnfavorite.forEach((btn) => {
+// ADD TO FAVORITES
+btnFavorite.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const id = e.target.dataset.id;
-    const url = `/api/events/${id}/favorite`;
-    const method = "POST";
-    fetch(url, { method })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.success) {
-          btn.classList.add("btn-success");
-          btn.innerHTML = "Favorite";
+    // console.log("id", id);
+
+    fetch("/api/favorites", {
+      method: "POST",
+      body: JSON.stringify({
+        event_id: parseInt(id),
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not OK");
         }
+        return response.json(); // Parse the response body as JSON
+      })
+      // Here, 'data' contains the parsed response data
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.error(err);
       });
   });
 });
