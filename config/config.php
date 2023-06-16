b97b817f95d1e2c86f49cc23f20ef1b19c00da50
Francisco Olivares Casillas <122823553+PacoCasillas@users.noreply.github.com>
2023-06-16 18:37:12 -0400

    Delete config.php
    Signed-off-by: Francisco Olivares Casillas <122823553+PacoCasillas@users.noreply.github.com>

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db_host = "us-cdbr-east-06.cleardb.net";
-$db_user = "bae20ef9676297";
-$db_pass = "03a3286c";
-$db_name = "heroku_49af44143a45886";
-
-$connect = mysqli_connect ($db_host, $db_user, $db_pass, $db_name) or die("database connection error");
